05f1790665f306e7fe942e65df6f58ce45c4cbbc
System Administrator <root@Traviss-Laptop.local>
2024-11-03 17:48:58 +0600

    Quartz sync: Nov 3, 2024, 5:48 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Bulletproof Discipleship",
+    pageTitle: "Bulletproof \nDiscipleship",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
